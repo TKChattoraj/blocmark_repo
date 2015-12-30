024a0381d55b817d7e83febafc66be7e7fd5a2d2
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-12-30 14:01:51 -0500

    establish sign-up, sign-in, sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { confirmations: "users/confirmations", passwords: "users/passwords", registrations: "users/registrations", sessions: "users/sessions", unlocks: "users/unlocks"}
+
   get 'welcome/index'
 
   get 'welcome/about'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
   random = rand(1000)
   factory :user do
     user_name "User_Name_#{random}"
-    email "#{user_name}@factory.com"
+    email {"#{user_name}@factory.com"}
     password "hello_world_#{random}"
-    password "hello_world_#{random}"
+    password_confirmation "hello_world_#{random}"
   end
 end
