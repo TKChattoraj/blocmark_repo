9db2150163e702728701f8a2cab1cde53bf8799f
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2016-02-14 15:38:47 -0500

    clean up Topics Controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -34,25 +34,6 @@ class TopicsController < ApplicationController
 
   end
 
-  # def edit
-  #   @topic = Topic.find(params[:id])
-  #   @user = User.find(@topic.user_id)
-  # end
-  #
-  # def update
-  #   @topic = Topic.find(params[:id])
-  #   @topic.assign_attributes(topic_params)
-  #
-  #   if @topic.save
-  #     flash[:notice] = "Topic was successfully updated"
-  #     redirect_to topic_path(@topic.id)
-  #   else
-  #     flash[:error] = "There was an error in updating the Topic.  Please try again."
-  #     render :edit
-  #   end
-  #
-  # end
-
   def destroy
     @topic = Topic.find(params[:id])
 
